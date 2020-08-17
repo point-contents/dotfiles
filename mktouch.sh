bd5629d6205a7cb2a0df26821d130e58d2682f50
arikiv <59433949+point-contents@users.noreply.github.com>
2020-08-17 17:35:20 +1200

    another wee helper function for the terminal

---
+++ mktouch.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+#this will make a file in a directory that
+#doesnt yet exist
+
+mktouch () {
+	mkdir -p $(dirname $1) && touch $1
+}
+
+## For example
+## mktouch /Documents/code/streamio.h
+## would make a new folder called "code" assuming it 
+## doesnt already exist.
+## and put a new file in that folder called stream io
+
+
