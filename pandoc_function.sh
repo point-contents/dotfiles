d66dd1240da1fd6515c80d3c2ec9747c1c57fa50
arikiv <59433949+point-contents@users.noreply.github.com>
2020-08-17 15:18:51 +1200

    add a pandoc funtion to save my hands

---
+++ pandoc_function.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Quick function/alias to make a templated pandoc latex file
+# and hopefully catch any errors in compiling
+# just call pandy and the name of the markdown file 
+
+
+pandy () {
+	pdffile=${1%.md}.pdf
+	echo "Now turning $1 into a beautiful pdf"
+	pandoc -f markdown -t latex "$1" -o $pdffile --template=eisvogel 2>failure
+	result=$?
+	if [ $result -eq 0 ]; then
+		echo "The file is called $pdffile"
+	else
+		echo -e "That was unfortunately a failure, further information below \n"
+		echo -e "$(cat failure)" "\n"
+		echo -e "If the error message does not provide enough information"
+		echo -e "then you should call pandoc fully to look at the latex log \n"
+
+	fi
+	rm failure
+}
+
